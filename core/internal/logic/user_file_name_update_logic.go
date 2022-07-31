d465fbb40ce1229b1d3cfe178124adca093a7c0f
callmePicacho <6519288+callmepicacho@user.noreply.gitee.com>
2022-07-31 13:56:46 +0800

    fix: 文件修改前先检查同级目录下是否存在相同文件名称

--- core/internal/logic/user_file_name_update_logic.go
+++ core/internal/logic/user_file_name_update_logic.go
@@ -3,6 +3,7 @@ package logic
 import (
 	"cloud-disk/core/models"
 	"context"
+	"errors"
 
 	"cloud-disk/core/internal/svc"
 	"cloud-disk/core/internal/types"
@@ -25,6 +26,17 @@ func NewUserFileNameUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext)
 }
 
 func (l *UserFileNameUpdateLogic) UserFileNameUpdate(req *types.UserFileNameUpdateRequest, userIdentity string) (resp *types.UserFileNameUpdateReply, err error) {
+	// 检查文件同级目录下是否存在相同文件名称
+	var cnt int64
+	err = l.svcCtx.Engine.Model(&models.UserRepository{}).Where("user_identity = ? and name = ? and parent_id = (select parent_id from user_repository ur where ur.identity = ?)", userIdentity, req.Name, req.Identity).
+		Count(&cnt).Error
+	if err != nil {
+		return
+	}
+	if cnt > 0 {
+		return nil, errors.New("该名称已存在")
+	}
+	// 修改文件名称
 	err = l.svcCtx.Engine.Model(&models.UserRepository{}).
 		Where("identity = ? AND user_identity = ?", req.Identity, userIdentity).
 		Update("name", req.Name).Error
